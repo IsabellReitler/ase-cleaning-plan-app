45b06998b807753a98cedbb5ee9c931597283dd4
Isabell Reitler <isabell.reitler@gmx.de>
2021-02-18 08:17:31 +0000

    WIP Add Tabs for to do lists

--- src/app/src/main/java/de/reitler/cleaning_plan_android_client/ui/todolist/ToDoListFragment.java
+++ src/app/src/main/java/de/reitler/cleaning_plan_android_client/ui/todolist/ToDoListFragment.java
@@ -9,27 +9,65 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.viewpager.widget.ViewPager;
+
+import com.google.android.material.tabs.TabLayout;
 
 import de.reitler.cleaning_plan_android_client.R;
+import de.reitler.cleaning_plan_android_client.ui.todolist.daily.DailyToDoListFragment;
 
 public class ToDoListFragment extends Fragment {
 
     private ToDoListViewModel toDoListViewModel;
+    private View view;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         toDoListViewModel =
                 new ViewModelProvider(this).get(ToDoListViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_todolist, container, false);
-        final TextView textView = root.findViewById(R.id.text_home);
+        view = inflater.inflate(R.layout.fragment_todolist, container, false);
         toDoListViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
-                textView.setText(s);
             }
         });
-        return root;
+        return view;
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        DemoCollectionPagerAdapter demoCollectionPagerAdapter = new DemoCollectionPagerAdapter(getChildFragmentManager());
+        ViewPager viewPager = view.findViewById(R.id.pager);
+        viewPager.setAdapter(demoCollectionPagerAdapter);
+        TabLayout tabLayout = view.findViewById(R.id.tab_layout);
+        tabLayout.setupWithViewPager(viewPager);
+    }
+}
+class DemoCollectionPagerAdapter extends FragmentStatePagerAdapter {
+
+    public DemoCollectionPagerAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public int getCount() {
+        return 2;
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "OBJECT " + (position + 1);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return new DailyToDoListFragment();
+    }
+
 }
---
+++ src/app/src/main/java/de/reitler/cleaning_plan_android_client/ui/todolist/daily/DailyToDoListViewModel.java
@@ -0,0 +1,7 @@
+package de.reitler.cleaning_plan_android_client.ui.todolist.daily;
+
+import androidx.lifecycle.ViewModel;
+
+public class DailyToDoListViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
