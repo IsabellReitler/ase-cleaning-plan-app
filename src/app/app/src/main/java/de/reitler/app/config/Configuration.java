1109b275382d228995173f1b5a0b72d06c770be7
Isabell Reitler <isabell.reitler@gmx.de>
2021-05-30 19:32:19 +0200

    Add comment to Productiv server address

--- src/app/app/src/main/java/de/reitler/app/config/Configuration.java
+++ src/app/app/src/main/java/de/reitler/app/config/Configuration.java
@@ -1,6 +1,8 @@
 package de.reitler.app.config;
 
 public class Configuration {
+
+    //Addresse: Producitv Server
     public static String BASE_URL = "http://otbydfc3ppajlmcm.myfritz.net:8080";
     public static String CLIENT_ID = "852266638483-6cv7osvpfo2fpf700fsv9m97kb1oim40.apps.googleusercontent.com";
 }
